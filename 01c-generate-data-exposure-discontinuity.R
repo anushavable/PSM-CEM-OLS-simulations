fb4270a869aa581d1f17390aa15ec2255790c9f1
Mathew Kiang <mathew.kiang@gmail.com>
2018-01-28 15:47:19 -0500

    Linting 01c

--- 01c-generate-data-exposure-discontinuity.R
+++ 01c-generate-data-exposure-discontinuity.R
@@ -1,36 +1,41 @@
-# This code creates the 10,000 datasets for the "exposure discontinuity" data structure. 
-# We generate all 10,000 datasets at once such that each column is a different dataset 
-# with 80,000 observations (i.e. 20,000 observations for each of our 4 variables),
-# and then separate each column into a dataset in Stata. 
+# This code creates the 10,000 datasets for the "exposure discontinuity" data 
+# structure. We generate all 10,000 datasets at once such that each column is 
+# a different dataset with 80,000 observations (i.e. 20,000 observations for 
+# each of our 4 variables), and then separate each column into a dataset 
+# in Stata.
 
 setwd("")
-#install.packages('arm')
+# install.packages('arm')
 
 library(arm)
-iter = 10000
-obs = 20000
+iter <- 10000
+obs  <- 20000
 
 # generate confounders
-x1 = rnorm(iter*obs)
-x2 = x1 + 3.85*rnorm(iter*obs)
+x1 <- rnorm(iter * obs)
+x2 <- x1 + 3.85 * rnorm(iter * obs)
 
-# generate exposure 
-a = NA # creates vector 
-a[x1<0.5] = rbinom(length(a[x1<0.5]), 1, 0.5) # when x1 < 0.5, samples from binomial dist with pr(1=0.5)
-a[x1>=0.5] = rbinom(length(a[x1>=0.5]), 1, 0.95) # when x1 >=0.5, samples from binomial dist with pr(1=0.95)
-mean(a[x1<0.5]) #should be around 0.5
-mean(a[x1>=0.5]) # should be around 0.95
+# generate exposure
+a <- NA
+# when x1 < 0.5, samples from binomial dist with pr(1=0.5)
+a[x1 < 0.5] <- rbinom(length(a[x1 < 0.5]), 1, 0.5) 
+
+# when x1 >=0.5, samples from binomial dist with pr(1=0.95)
+a[x1 >= 0.5] <- rbinom(length(a[x1 >= 0.5]), 1, 0.95) 
+
+mean(a[x1 < 0.5])  # should be around 0.5
+mean(a[x1 >= 0.5]) # should be around 0.95
 
 # generate outcome
-y = NA 
-y = 0*a + 110*x1 + 10*x2 + rnorm(iter*obs)
+y <- NA
+y <- 0 * a + 110 * x1 + 10 * x2 + rnorm(iter * obs)
 
-x1 = matrix(x1,ncol=iter)
-x2 = matrix(x2,ncol=iter)
-a = matrix(a,ncol=iter)
-y = matrix(y,ncol=iter)
-cormat = diag(cor(x1,x2))
+x1 <- matrix(x1, ncol = iter)
+x2 <- matrix(x2, ncol = iter)
+a  <- matrix(a, ncol = iter)
+y  <- matrix(y, ncol = iter)
+cormat <- diag(cor(x1, x2))
 
-# write 10,000 datasets to csv file to import into Stata 
+# write 10,000 datasets to csv file to import into Stata
 exp_discon <- rbind(x1, x2, a, y)
 write.csv(exp_discon, file = "sims.csv")
